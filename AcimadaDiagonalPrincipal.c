8013bcb8207050c9d94017f7a09112a922ed9a9a
augustomerigo <augustomerigobattisti@gmail.com>
2025-06-16 10:32:00 -0300

    Create AcimadaDiagonalPrincipal.c

---
+++ AcimadaDiagonalPrincipal.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    char op;
+    double M[12][12];
+    double sum = 0.0;
+    int count = 0;
+    scanf(" %c", &op);
+    for (int i = 0; i < 12; i++) {
+        for (int j = 0; j < 12; j++) {
+            scanf("%lf", &M[i][j]);
+        }
+    }
+    for (int i = 0; i < 12; i++) {
+        for (int j = i + 1; j < 12; j++) {
+            sum += M[i][j];
+            count++;
+        }
+    }
+    if (op == 'S') {
+        printf("%.1lf\n", sum);
+    } else if (op == 'M') {
+        printf("%.1lf\n", sum / count);
+    }
+
+    return 0;
+}
